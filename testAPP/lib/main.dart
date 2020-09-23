95d82a749a374405f8a2d19e8861b0508c4c5684
devdutt <devdikshit12350@gmail.com>
2020-09-23 17:12:25 +0530

    Material Scaffold App done

--- testAPP/lib/main.dart
+++ testAPP/lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text("hello", textDirection: TextDirection.ltr),
-  ));
+  runApp(new Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My First App",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home Page"),
+        ),
+        body: new Center(
+          child: new Text("WELCOME USER"),
+        ),
+      ),
+    );
+  }
 }
